1ef181ea1c553a209cda4f9b1d1637da510bedb9
Dragoș Șerban <dragos4002@gmail.com>
2025-03-12 20:45:53 +0200

    improveed Pagination component layout and button styles

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,29 +1,38 @@
-export default function Pagination({currentPage, totalPages, setCurrentPage}){
-    return(
-        <div class="fixed bottom-4 left-4 justify-center items-center mt-6 space-x-4">
-        <button
-          onClick={() => setCurrentPage(currentPage - 1)}
-          disabled={currentPage === 1}
-          class={
-            'px-4 py-2 font-bold text-black ${currentPage === 1 ? "bg-gray-400 cursor-not-allowed" : "bg-black text-white"} rounded-lg'
-          }
-        >
-          ← Previous
-        </button>
+export default function Pagination({
+  currentPage,
+  totalPages,
+  setCurrentPage,
+}) {
+  return (
+    <div class="fixed bottom-2 left-2 justify-center items-center mt-6 space-x-4">
+      {/* Previous Button */}
+      <button
+        onClick={() => setCurrentPage(currentPage - 1)}
+        disabled={currentPage === 1}
+        class={
+          "px-4 py-2 font-bold text-black rounded-lg " +
+          (currentPage === 1 ? "text-gray-500" : "cursor-pointer  text-black")
+        }
+      >
+        {"<--"} Previous
+      </button>
 
-        <span class="text-lg font-semibold text-black">
-          Page {currentPage} of {totalPages}
-        </span>
+      {/* Page Counter */}
+      <span class="text-lg font-semibold text-black">
+        Page {currentPage} of {totalPages}
+      </span>
 
-        <button
-          onClick={() => setCurrentPage(currentPage + 1)}
-          disabled={currentPage === totalPages}
-          class={
-            'px-4 py-2 font-bold text-black ${currentPage === totalPages ? "bg-gray-400 cursor-not-allowed" : "bg-black text-white"} rounded-lg'
-          }
-        >
-          Next →
-        </button>
-      </div>
-    );
+      {/* Next Button */}
+      <button
+        onClick={() => setCurrentPage(currentPage + 1)}
+        disabled={currentPage === totalPages}
+        class={
+          "px-4 py-2 font-bold text-black rounded-lg " +
+          (currentPage === totalPages ? "text-gray-500" : "cursor-pointer  text-black")
+        }
+      >
+        Next {"-->"}
+      </button>
+    </div>
+  );
 }

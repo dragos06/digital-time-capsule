5b9bbc52c5906da824a7686de87d63315d04bdc6
Dragoș Șerban <dragos4002@gmail.com>
2025-03-16 19:39:20 +0200

    remove Courier New font from CapsulesGrid component

--- src/components/CapsulesGrid.js
+++ src/components/CapsulesGrid.js
@@ -2,7 +2,7 @@ import CapsuleCard from "./CapsuleCard";
 
 export default function CapsulesGrid({ capsules }) {
   return (
-    <div class="grid grid-cols-3 gap-6 px-5 py-8 font-courier text-black">
+    <div class="grid grid-cols-3 gap-6 px-5 py-8 text-black">
       {capsules.map((capsule) => (
         <CapsuleCard key={capsule.id} capsule={capsule} />
       ))}

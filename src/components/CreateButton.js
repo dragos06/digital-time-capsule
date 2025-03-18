ada046f3c286bbdd16201b339709dfea0c07df98
Dragoș Șerban <dragos4002@gmail.com>
2025-03-18 18:53:53 +0200

    refactored CreateButton to use modal for capsule creation

--- src/components/CreateButton.js
+++ src/components/CreateButton.js
@@ -1,13 +1,30 @@
-import Link from "next/link";
+import { useState } from "react";
+import CreateCapsuleModal from "./CreateCapsuleModal";
 
-export default function CreateButton() {
+export default function CreateButton({ onAdd }) {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClose = () => setShowModal(false);
+  const handleAdd = (name, date, message) => {
+    onAdd(name, date, message);
+    setShowModal(false);
+  }
+
   return (
-    <div>
-      <Link href="/create">
-        <button class="cursor-pointer px-4 py-2 text-xl font-bold bg-[#D9D9D9] text-black rounded-4xl drop-shadow-lg">
-          + Create New Capsule
-        </button>
-      </Link>
+    <div className="relative">
+      <button
+        onClick={() => setShowModal(true)} // Show modal when button is clicked
+        className="cursor-pointer px-4 py-2 text-xl font-bold bg-[#D9D9D9] text-black rounded-4xl drop-shadow-lg"
+      >
+        + Create New Capsule
+      </button>
+
+      {/* Render the CreateCapsuleModal */}
+      <CreateCapsuleModal
+        isOpen={showModal} // Modal visibility controlled by showModal state
+        onClose={handleClose} // Pass handleClose to close modal
+        onAdd={handleAdd} // Pass handleAdd to add new capsule
+      />
     </div>
   );
 }

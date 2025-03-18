d3abd35f5e4c5361d830ae1005436f3ab9bfca33
Dragoș Șerban <dragos4002@gmail.com>
2025-03-18 18:53:45 +0200

    passed delete functionality to CapsuleCard component in CapsulesGrid

--- src/components/CapsulesGrid.js
+++ src/components/CapsulesGrid.js
@@ -1,10 +1,10 @@
 import CapsuleCard from "./CapsuleCard";
 
-export default function CapsulesGrid({ capsules }) {
+export default function CapsulesGrid({ capsules, onDelete }) {
   return (
-    <div class="grid grid-cols-3 gap-6 px-5 py-8 text-black">
+    <div className="grid grid-cols-3 gap-6 px-5 py-8 text-black">
       {capsules.map((capsule) => (
-        <CapsuleCard key={capsule.id} capsule={capsule} />
+        <CapsuleCard key={capsule.id} capsule={capsule} onDelete={onDelete}/>
       ))}
     </div>
   );

0a9c09f80b169f9099f8264c319e7251c744682a
Dragoș Șerban <dragos4002@gmail.com>
2025-03-18 18:54:09 +0200

    fix: change class to className in Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <h1 class="bg-[#D9D9D9] text-4xl h-[60px] text-black text-center p-3 shadow-lg">
+    <h1 className="bg-[#D9D9D9] text-4xl h-[60px] text-black text-center p-3 shadow-lg">
       Digital Time Capsule
     </h1>
   );

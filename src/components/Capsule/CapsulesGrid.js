d4a29eb7073e9ca1e3b68fb69c6d15d70f44b09c
Dragoș Șerban <dragos4002@gmail.com>
2025-03-28 17:47:51 +0200

    added CapsulesGrid component to display a grid of CapsuleCard components

---
+++ src/components/Capsule/CapsulesGrid.js
@@ -0,0 +1,11 @@
+import CapsuleCard from "./CapsuleCard";
+
+export default function CapsulesGrid({ capsules, onDelete }) {
+  return (
+    <div className="grid grid-cols-3 gap-6 px-5 py-8 text-black">
+      {capsules.map((capsule) => (
+        <CapsuleCard key={capsule.id} capsule={capsule} onDelete={onDelete}/>
+      ))}
+    </div>
+  );
+}

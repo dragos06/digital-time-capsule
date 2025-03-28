222352c01ce862c85d31c03bdc57e09828ae8a3f
Dragoș Șerban <dragos4002@gmail.com>
2025-03-28 17:48:17 +0200

    added Header component for the Digital Time Capsule application

---
+++ src/components/UI/Header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <h1 className="bg-[#D9D9D9] text-4xl h-[60px] text-black text-center p-3 shadow-lg">
+      Digital Time Capsule
+    </h1>
+  );
+}

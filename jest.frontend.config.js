c9054c71f91807aead9d955462cef5dc07477cb9
Dragoș Șerban <dragos4002@gmail.com>
2025-04-05 13:36:49 +0300

    added Jest configuration for backend and frontend testing, update test scripts in package.json

---
+++ jest.frontend.config.js
@@ -0,0 +1,12 @@
+export default {
+    displayName: "frontend",
+    testEnvironment: "jsdom",
+    testMatch: ["<rootDir>/__test__/frontend/*.test.js"],
+    transform: {
+      "^.+\\.jsx?$": "babel-jest",
+    },
+    moduleNameMapper: {
+      "^@/(.*)$": "<rootDir>/src/$1",
+    },
+  };
+  

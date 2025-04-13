5bc4dba1100d16bdba56d75576052a8d9c43b6bb
Dragoș Șerban <dragos4002@gmail.com>
2025-04-13 23:13:24 +0300

    Fix capsule ID reference in CapsulesGrid component

--- src/components/Capsule/CapsulesGrid.js
+++ src/components/Capsule/CapsulesGrid.js
@@ -4,7 +4,7 @@ export default function CapsulesGrid({ capsules, onDelete }) {
   return (
     <div className="grid grid-cols-3 gap-6 px-5 py-8 text-black">
       {capsules.map((capsule) => (
-        <CapsuleCard key={capsule.id} capsule={capsule} onDelete={onDelete}/>
+        <CapsuleCard key={capsule.capsule_id} capsule={capsule} onDelete={onDelete}/>
       ))}
     </div>
   );

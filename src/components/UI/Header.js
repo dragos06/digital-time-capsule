1fb2b1de54848886f1bbb385c0e18697f6c7a01e
Dragoș Șerban <dragos4002@gmail.com>
2025-04-18 18:48:58 +0300

    Update Header component: add bottom border for improved visual separation

--- src/components/UI/Header.js
+++ src/components/UI/Header.js
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <h1 className="bg-[#D9D9D9] text-4xl h-[60px] text-black text-center p-3 shadow-lg">
+    <h1 className="bg-[#D9D9D9] text-4xl h-[60px] text-black text-center p-3 shadow-lg border-b-2">
       Digital Time Capsule
     </h1>
   );

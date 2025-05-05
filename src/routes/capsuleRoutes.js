fe97fb730dcc4c591f64ebf9cf66be4cbfaa676e
Dragoș Șerban <dragos4002@gmail.com>
2025-05-05 15:23:40 +0300

    feat: add endpoint for fetching top capsules statistics

--- src/routes/capsuleRoutes.js
+++ src/routes/capsuleRoutes.js
@@ -1,6 +1,13 @@
 import express from "express";
-import {getCapsulesController, getCapsuleByIdController, createCapsuleController, updateCapsuleController, deleteCapsuleController} from "../controllers/capsuleController.js";
+import {
+  getCapsulesController,
+  getCapsuleByIdController,
+  createCapsuleController,
+  updateCapsuleController,
+  deleteCapsuleController,
+} from "../controllers/capsuleController.js";
 import validateCapsule from "../middlewares/validateCapsule.js";
+import pool from "../db/pool.js"
 
 const router = express.Router();
 
@@ -9,5 +16,23 @@ router.get("/:id", getCapsuleByIdController);
 router.post("/", validateCapsule, createCapsuleController);
 router.put("/:id", validateCapsule, updateCapsuleController);
 router.delete("/:id", deleteCapsuleController);
+
+router.get("/stats/top-capsules", async (req, res) => {
+  try {
+    const { rows } = await pool.query(`
+            SELECT tc.capsule_id, tc.capsule_title, SUM(m.file_size) AS total_size
+            FROM time_capsules tc
+            JOIN memories m ON tc.capsule_id = m.capsule_id
+            WHERE tc.capsule_status = 'Unlocked'
+            AND m.uploaded_at > CURRENT_DATE - INTERVAL '3 months'
+            GROUP BY tc.capsule_id, tc.capsule_title
+            ORDER BY total_size DESC
+            LIMIT 50;`);
+    res.json(rows);
+  } catch (err) {
+    console.error("Error fetching stats:", err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
 export default router;

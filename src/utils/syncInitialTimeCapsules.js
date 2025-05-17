00881fccfa834e2e1e42d09bc1776769f423e0d6
Dragoș Șerban <dragos4002@gmail.com>
2025-05-17 23:59:20 +0300

    Separated frontend from backend, removed redundant files

--- src/utils/syncInitialTimeCapsules.js
+++
@@ -1,4 +0,0 @@
-export const syncInitialTimeCapsules = (newCapsules, initialTimeCapsules) => {
-    initialTimeCapsules.length = 0; // Clear the original data
-    initialTimeCapsules.push(...newCapsules); // Add the updated data
-  };
